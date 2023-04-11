cc6c4d00c51beb01aaa8d81f8daf491b5b0fa729
myusername <myemail@example.com>
2023-04-11 15:37:43 -0400

    Committing changes

--- custom_tools.py
+++ custom_tools.py
@@ -109,7 +109,7 @@ class GitToolBuilder:
             title = "Fix for issue #" + str(self.work_item.number)
             pull: github.PullRequest.PullRequest = self.github_repo.create_pull(
                 head=current_branch,
-                base=self.github_repo.default_branch().name,
+                base=self.github_repo.default_branch,
                 issue=self.work_item,
             )
             if self.logger:
