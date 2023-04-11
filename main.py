54e4ca37521d80edab92cf361a79624bfd18e9e9
myusername <myemail@example.com>
2023-04-10 20:58:52 -0400

    break out planning and executing agents

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from github.Issue import Issue
 from github.PullRequest import PullRequest
 from langchain.agents import initialize_agent, load_tools, AgentType
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 from config import *
 from custom_tools import GitToolBuilder
+from prompts import make_planning_task, make_execution_task
 from utils import login_github, list_repositories, choose_work_item, PassThroughBuffer
 
 # Log into GitHub
@@ -29,9 +31,9 @@ github_repo = github_client.get_repo(repository_name)
 # create a repo object which represents the repository we are inside of
 pygit_repo = Repo(os.getcwd())
 
-# reset to default branch if necessary
-if pygit_repo.active_branch.name != "main":
-    pygit_repo.git.checkout("main")
+# # reset to default branch if necessary
+# if pygit_repo.active_branch.name != "main":
+#     pygit_repo.git.checkout("main")
 
 # checkout default branch and pull
 pygit_repo.git.checkout("main")
@@ -40,63 +42,69 @@ pygit_repo.git.pull()
 work_item = choose_work_item(github_repo)
 
 
-llm = ChatOpenAI(temperature=0.01, model="gpt-4")
+llm = ChatOpenAI(temperature=0, model="gpt-4")
 # llm1 = OpenAI(temperature=0)
 pass_through_buffer = PassThroughBuffer(sys.stdout)
 assert pass_through_buffer.saved_output == ""
 sys.stdout = pass_through_buffer
-tools = load_tools(["python_repl", "terminal", "serpapi", "requests_get"], llm=llm)
-tools += GitToolBuilder(github_repo, pygit_repo, work_item).build_tools()
+base_tools = load_tools(["python_repl", "terminal", "serpapi", "requests_get"], llm=llm)
+exec_tools = (
+    base_tools + GitToolBuilder(github_repo, pygit_repo, work_item).build_tools()
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history")
+
+
+plan_agent = initialize_agent(
+    base_tools, llm, agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION, verbose=True, memory=memory
+)
 
 exec_agent = initialize_agent(
-    tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+    exec_tools, llm, agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION, verbose=True
 )
 
+# check if instrutions are already attached to the issue
+instructions = [c.body for c in work_item.get_comments() if c.body.startswith(PLANNER_AGENT_OUTPUT_STRING)]
+if instructions:
+    instructions = instructions.pop()
+    instructions.replace(PLANNER_AGENT_OUTPUT_STRING, "")
+    print("Found instructions:", instructions)
 
-task = (
-    f"You are a GPT-4-powered software engineer agent."
-    f" You are built with langchain, a framework for building language-based agents. "
-    f" You can read about it here: https://python.langchain.com/en/latest/modules/agents.html"
-    f" Your task is to contribute clean, high-quality code to the given codebase."
-    f" You are working in {os.getcwd()} on {github_repo.name} repository."
-    f" Feel free to look around this repo. You can browse the internet for documentation and examples."
-    f" You may need to create, modify, or delete one or more files in this repository."
-)
+# ask user if they want to run planner agent
+do_plan = input("Do you want to run the planner agent? (y/n)")
 
-if type(work_item) == Issue:
-    task += f" You must create a new branch, implement a solution and submit a pull request to address the following issue: \n\n Title: {work_item.title}.\n\n Body: {work_item.body}."
-if type(work_item) == PullRequest:
-    task += (
-        f" You must checkout the branch, understand the following pull request feedback make a commit with changes to address it:"
-        f" \n\n Title: {work_item.title}.\n\n Body: {work_item.body}. \n\n Files: {[f.filename for f in work_item.get_files()]}:"
-    )
+if do_plan == "y":
+    plan_task = make_planning_task(work_item, exec_tools, github_repo.name)
+    print("Planning task:", plan_task)
+    approved = False
+    while not approved:
+        instructions = plan_agent.run(plan_task)
+        print("Created new Instructions:", instructions)
+        feedback = input("Do you approve? If approved, type 'y'. If not approved, type why so the agent can try again")
+        approved = feedback == "y"
+        plan_task = feedback
 
-
-comments = work_item.get_comments()
-
-if list(comments):
-    task += f" Comments:"
-    for comment in comments:
-        task += f" {comment.body}"
+    # save instructions to issue
+    work_item.create_comment(PLANNER_AGENT_OUTPUT_STRING + instructions)
 
 
-task += (
-    f"\n\nUseful tips: Don't use nano, vim or other text editors, but rather modify files directly either via python or terminal. "
-    f" Before creating a new branch, make sure to pick a name that is not taken."
-)
 
-try:
-    print("Task:", task)
-    x = exec_agent.run(task)
-except ValueError as e:
-    if DO_RETRY:
-        tb = traceback.format_exc()
-        task += f" This is your second attempt. During the previous attempt, you crashed with the following sequence: <run>{pass_through_buffer.saved_output}</run> Let's try again, avoiding previous mistakes."
-        pass_through_buffer.saved_output = ""
-        print("Failed to complete task with following error:", e, tb)
-        print("New task:", task)
-        print("Retrying...")
-        x = exec_agent.run(task)
-finally:
-    sys.stdout = pass_through_buffer.original_buffer
-    pygit_repo.git.checkout("main")
+# ask user if they want to run exec agent
+do_exec = input("Do you want to run the execution agent? (y/n)")
+if do_exec == "y":
+    exec_task = make_execution_task(work_item, instructions, github_repo.name)
+    print("Execution task:", exec_task)
+    try:
+        exec_agent.run(exec_task)
+    except ValueError as e:
+        if DO_RETRY:
+            tb = traceback.format_exc()
+            exec_task += f" This is your second attempt. During the previous attempt, you crashed with the following sequence: <run>{pass_through_buffer.saved_output}</run> Let's try again, avoiding previous mistakes."
+            pass_through_buffer.saved_output = ""
+            print("Failed to complete execution task")
+            print("New task:", exec_task)
+            print("Retrying...")
+            exec_agent.run(exec_task)
+    finally:
+        sys.stdout = pass_through_buffer.original_buffer
+        pygit_repo.git.checkout("main")
---
+++ prompts.py
@@ -0,0 +1,65 @@
+import os
+from typing import List, Union
+
+from github.Issue import Issue
+from github.PullRequest import PullRequest
+from langchain.tools import BaseTool
+
+from config import PLANNER_AGENT_OUTPUT_STRING
+
+
+def make_planning_task(
+    work_item: Union[Issue, PullRequest], exec_tools: List[BaseTool], github_repo_name: str
+):
+    return (
+        f"You are a GPT-4 software engineering lead agent."
+        f" You are built with langchain, a framework for building language-based agents. "
+        f" You can read about it here: https://python.langchain.com/en/latest/modules/agents.html"
+        f" You are working in {os.getcwd()} on {github_repo_name} repository."
+        f" Your task is to thoroughly understand the following work item and "
+        f" create simple and thorough step-by-step instructions for a developer to implement the solution."
+        f" \n\nTitle: {work_item.title}"
+        f" \n\nBody: {work_item.body}"
+        f" \n\nComments: {[c.body for c in work_item.get_comments() if not c.body.startswith(PLANNER_AGENT_OUTPUT_STRING)]}"
+        f" \n\n The developer will use your instructions to make changes to the repository and"
+        f" submit a pull request"
+        if isinstance(work_item, Issue)
+        else "make a commit "
+        f" with working, clean, and documented code."
+        f" Your developer is also a GPT-4-powered agent, so keep that in mind when creating instructions."
+        f" You should acquire an excellent understanding of the current state of the repository and the code within it."
+        f" You should also look up documentation on the internet whenever necessary."
+        f" Your instructions should be clear and concise, and should not contain any typos or grammatical errors."
+        f" You should tell the developer which files to create/modify/delete, and what to write in them."
+        f" You should also tell the developer which external libraries to use, if any."
+        f" For external libraries, you should provide a link to the documentation."
+        f" Make sure not to regress any existing functionality."
+        f" The developer agent will have access to the following tools: {[(tool.name, tool.description) for tool in exec_tools]}, so keep that in mind when creating instructions."
+        f" Begin."
+    )
+
+
+def make_execution_task(
+    work_item: Union[Issue, PullRequest],
+    solution_instructions: str,
+    github_repo_name: str,
+):
+    return (
+        f"You are a GPT-4-powered software engineer agent."
+        f" You are built with langchain, a framework for building language-based agents. "
+        f" You can read about it here: https://python.langchain.com/en/latest/modules/agents.html"
+        f" Your task is to contribute clean, high-quality code to the given codebase."
+        f" You are working in {os.getcwd()} on {github_repo_name} repository."
+        f" You are working on the following work item: "
+        f"\n\nTitle: {work_item.title};"
+        f"\n\nBody: {work_item.body};"
+        f"\n\nComments: {[c.body for c in work_item.get_comments() if not c.body.startswith(PLANNER_AGENT_OUTPUT_STRING)]};"
+        f"\n\n A planning agent has created the following step-by-step instructions for you: <instructions>{solution_instructions}</instructions>"
+        f" Execute the instructions and"
+        f" create a pull request with your changes."
+        if isinstance(work_item, Issue)
+        else f" make a commit with your changes to the appropriate branch."
+        f" make sure not to regress any existing functionality."
+        f"\n\nUseful tips: Don't use nano, vim or other text editors, but rather modify files directly either via python or terminal. "
+        f" Before creating a new branch, make sure to pick a name that is not taken."
+    )
