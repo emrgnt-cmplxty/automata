9dc490c0944df7a3f2074104aae28fb6e2a9f11b
myusername <myemail@example.com>
2023-04-15 15:12:35 -0400

    Type annotation

--- spork/utils.py
+++ spork/utils.py
@@ -88,7 +88,7 @@ def choose_work_item(
     """
 
     choice = input("Do you want to work on issues or pull requests? (i/p)")
-    work_items = []
+    work_items: PaginatedList
 
     if choice == "i":
         work_items = list_issues(github_repo)
