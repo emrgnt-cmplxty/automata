9efd406889c93c8bb9485ef47b43e8611bbeef98
myusername <myemail@example.com>
2023-04-16 15:07:04 -0400

    Chmod

--- set_default_branch.sh
+++ set_default_branch.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 current_branch=$(git rev-parse --abbrev-ref HEAD)
-sed -i '' "s/^DEFAULT_BRANCH=.*/DEFAULT_BRANCH=$current_branch/" .env
+sed -i '' "s/^DEFAULT_BRANCH_NAME=.*/DEFAULT_BRANCH_NAME=$current_branch/" .env
