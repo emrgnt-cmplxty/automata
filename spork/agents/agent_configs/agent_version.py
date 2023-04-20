add77833c355dc1a1879ef012ae6142c4954d2c7
Owen Colegrove <owen@algofi.org>
2023-04-20 13:53:10 -0400

    Introduce simpler Meeseeks configs

--- spork/agents/agent_configs/agent_version.py
+++ spork/agents/agent_configs/agent_version.py
@@ -4,3 +4,4 @@ from enum import Enum
 class AgentVersion(Enum):
     MEESEEKS_MASTER_V1 = "meeseeks_master_v1"
     MEESEEKS_RETRIEVER_V1 = "meeseeks_retriever_v1"
+    MEESEEKS_WRITER_V1 = "meeseeks_writer_v1"
