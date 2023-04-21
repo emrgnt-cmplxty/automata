3a4769b0ad5e3212c714bdcff765acd36ef327ec
myusername <myemail@example.com>
2023-04-21 14:06:56 -0400

    fixing tokenizer warning

--- spork/agents/mr_meeseeks_agent.py
+++ spork/agents/mr_meeseeks_agent.py
@@ -158,7 +158,7 @@ class MrMeeseeksAgent:
                     words = accumulated_output.split(separator)
                     # Print all words except the last one, as it may be an incomplete word
                     for word in words[:-1]:
-                        print(colored(str(word), "cyan"), end=" ", flush=True)
+                        print(colored(str(word), "green"), end=" ", flush=True)
                     # Keep the last (potentially incomplete) word for the next iteration
                     accumulated_output = words[-1]
         else:
@@ -182,7 +182,9 @@ class MrMeeseeksAgent:
         logger.debug("Synthetic User Message:\n%s\n" % MrMeeseeksAgent.CONTINUE_MESSAGE)
         context_length = sum(
             [
-                len(self.tokenizer.encode(message["content"], max_length=1024 * 8))
+                len(
+                    self.tokenizer.encode(message["content"], max_length=1024 * 8, truncation=True)
+                )
                 for message in self.messages
             ]
         )
