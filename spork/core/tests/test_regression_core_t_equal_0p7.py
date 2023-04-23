f2341797204aa270e88f4736d9d3732f6589a34c
Owen Colegrove <owen@algofi.org>
2023-04-23 18:34:54 -0400

    Uncomment relevant files

--- spork/core/tests/test_regression_core_t_equal_0p7.py
+++ spork/core/tests/test_regression_core_t_equal_0p7.py
@@ -49,60 +49,60 @@ EXPECTED_RESPONSES = {
 }
 
 
-# @retry(3)
-# @pytest.mark.regression
-# @pytest.mark.parametrize(
-#     "mr_meeseeks_params",
-#     [
-#         {
-#             "model": MODEL,
-#             "temperature": TEMPERATURE,
-#             "tool_list": ["meeseeks_indexer", "meeseeks_writer"],
-#         },
-#         # Add more parameter sets as needed
-#     ],
-#     indirect=True,
-# )
-# def test_write_simple_function(mr_meeseeks_params):
-#     expected_content = EXPECTED_RESPONSES["test_write_simple_function"].strip()
-#     agent = build_agent_with_params(
-#         mr_meeseeks_params,
-#         f"Write the following function - '{expected_content}' to the file core.tests.sample_code.test",
-#         AgentVersion.MEESEEKS_WRITER_V2,
-#         max_iters=5,
-#         temperature=TEMPERATURE,
-#         model=MODEL,
-#     )
-#     agent.run()
-#     cleanup_and_check(expected_content, "test.py")
+@retry(3)
+@pytest.mark.regression
+@pytest.mark.parametrize(
+    "mr_meeseeks_params",
+    [
+        {
+            "model": MODEL,
+            "temperature": TEMPERATURE,
+            "tool_list": ["meeseeks_indexer", "meeseeks_writer"],
+        },
+        # Add more parameter sets as needed
+    ],
+    indirect=True,
+)
+def test_write_simple_function(mr_meeseeks_params):
+    expected_content = EXPECTED_RESPONSES["test_write_simple_function"].strip()
+    agent = build_agent_with_params(
+        mr_meeseeks_params,
+        f"Write the following function - '{expected_content}' to the file core.tests.sample_code.test",
+        AgentVersion.MEESEEKS_WRITER_V2,
+        max_iters=5,
+        temperature=TEMPERATURE,
+        model=MODEL,
+    )
+    agent.run()
+    cleanup_and_check(expected_content, "test.py")
 
 
-# @retry(3)
-# @pytest.mark.regression
-# @pytest.mark.parametrize(
-#     "mr_meeseeks_params",
-#     [
-#         {
-#             "model": MODEL,
-#             "temperature": TEMPERATURE,
-#             "tool_list": ["python_indexer", "python_writer"],
-#         },
-#         # Add more parameter sets as needed
-#     ],
-#     indirect=True,
-# )
-# def test_advanced_writer_example(mr_meeseeks_params):
-#     expected_content = EXPECTED_RESPONSES["test_advanced_writer_example"].strip()
-#     agent = build_agent_with_params(
-#         mr_meeseeks_params,
-#         f"Write the following module - '{expected_content}' to the file core.tests.sample_code.test2",
-#         version=AgentVersion.MEESEEKS_WRITER_V2,
-#         max_iters=2,
-#         temperature=TEMPERATURE,
-#         model=MODEL,
-#     )
-#     agent.run()
-#     cleanup_and_check(expected_content, "test2.py")
+@retry(3)
+@pytest.mark.regression
+@pytest.mark.parametrize(
+    "mr_meeseeks_params",
+    [
+        {
+            "model": MODEL,
+            "temperature": TEMPERATURE,
+            "tool_list": ["python_indexer", "python_writer"],
+        },
+        # Add more parameter sets as needed
+    ],
+    indirect=True,
+)
+def test_advanced_writer_example(mr_meeseeks_params):
+    expected_content = EXPECTED_RESPONSES["test_advanced_writer_example"].strip()
+    agent = build_agent_with_params(
+        mr_meeseeks_params,
+        f"Write the following module - '{expected_content}' to the file core.tests.sample_code.test2",
+        version=AgentVersion.MEESEEKS_WRITER_V2,
+        max_iters=2,
+        temperature=TEMPERATURE,
+        model=MODEL,
+    )
+    agent.run()
+    cleanup_and_check(expected_content, "test2.py")
 
 
 @retry(3)
