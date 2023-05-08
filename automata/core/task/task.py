7bfbeb09d1a5bdf86f3af17e903553c34f04e6ec
Owen Colegrove <owen@algofi.org>
2023-05-07 21:53:38 -0400

    Add more task logic

--- automata/core/task/task.py
+++ automata/core/task/task.py
@@ -1,15 +1,8 @@
-import logging
 import os
-import shutil
-import sqlite3
 import uuid
-import zipfile
 from enum import Enum
-from io import BytesIO
 from typing import Optional
 
-import requests
-from github import Github
 from retrying import retry
 
 from automata.config import DEFAULT_REMOTE_URL, GITHUB_API_KEY
@@ -17,39 +10,10 @@ from automata.core.agent.automata_agent_helpers import (
     create_builder_from_args,
     create_instruction_payload,
 )
+from automata.core.base.github_manager import GitHubManager
 from automata.core.utils import root_path
 
 JOB_DIR_NAME = "jobs"
-
-
-class GitHubManager:
-    def __init__(self, access_token: str, remote_url: str, remote_branch: str = "main"):
-        self.client = Github(access_token)
-        self.remote_url = remote_url
-        self.remote_branch = remote_branch
-
-    def clone_repository(self, local_path: str):
-        # Fetch the archive link from GitHub using the client
-        remote_name = self.remote_url.replace("https://github.com/", "")
-        repo = self.client.get_repo(remote_name)
-
-        # Fetch the repository archive
-        archive_url = repo.get_archive_link("zipball", ref=self.remote_branch)
-        response = requests.get(archive_url)
-
-        # Extract the contents of the ZIP file
-        with zipfile.ZipFile(BytesIO(response.content)) as zip_file:
-            zip_file.extractall()
-
-        # Move the contents of the extracted folder to a new folder with the repository name
-        for folder in os.listdir():
-            if folder.startswith(f"{repo.owner.login}-{repo.name}-"):
-                shutil.move(folder, local_path)
-                break
-
-    def sync_task(self, task):
-        # Sync task to GitHub (e.g., create/update issue, create/update file)
-        pass
 
 
 class TaskStatus(Enum):
@@ -60,33 +24,45 @@ class TaskStatus(Enum):
 
 
 class AutomataTask:
+    """
+    The AutomataTask class represents a task that is to be executed by the Automata agent.
+    """
+
     def __init__(self, github_manager: GitHubManager, *args, **kwargs):
         self.github_manager = github_manager
-        self.task_id = uuid.uuid4()  # Unique task ID
+        self.task_id = uuid.uuid4()
 
         self.priority = kwargs.get("priority", 0)
         self.max_retries = kwargs.get("max_retries", 3)
         self.status = TaskStatus(kwargs.get("status", "pending"))
+        self.rel_py_path = kwargs.get("rel_py_path", "automata")
 
         builder = create_builder_from_args(*args, **kwargs)
+
         self.agent = builder.build()
         self.result = None
         self.error: Optional[str] = None
         self.retry_count = 0
 
     def create_task_env(self):
+        """
+        Creates the environment for the task
+        """
         # Generate a unique directory name for the task
         task_dir_name = f"task_{self.task_id}"
-        self.job_dir = os.path.join(root_path(), JOB_DIR_NAME)
-        self.task_dir = os.path.join(self.job_dir, task_dir_name)
+        job_dir = os.path.join(root_path(), JOB_DIR_NAME)
 
         # Create the jobs directory if it does not exist
-        if not os.path.exists(self.job_dir):
-            os.makedirs(self.job_dir)
+        if not os.path.exists(job_dir):
+            os.makedirs(job_dir)
 
+        self.task_dir = os.path.join(job_dir, task_dir_name)
         self.github_manager.clone_repository(self.task_dir)
 
     def wait_exponential(self, retry_state):
+        """
+        Waits for a specified amount of time before retrying the task
+        """
         return (2**retry_state.attempt_number) * 1000  # Exponential backoff in milliseconds
 
     @retry(
@@ -95,127 +71,68 @@ class AutomataTask:
         stop_max_attempt_number=lambda self: self.max_retries,
     )
     def execute(self) -> Optional[str]:
-        self.create_task_env()
-        self.status = TaskStatus.RUNNING
-        try:
-            # self.result = self.agent.run()
-            self.status = TaskStatus.SUCCESS
-        except Exception as e:
-            logging.exception(f"AutomataTask failed: {e}")
-            self.error = str(e)
-            self.status = TaskStatus.FAILED
-            self.retry_count += 1
-            raise
-
-        return self.result
-
-
-class TaskDatabase:
-    def __init__(self, db_name: str):
-        self.conn = sqlite3.connect(db_name)
-        self.create_table()
-
-    def create_table(self) -> None:
-        cursor = self.conn.cursor()
-        cursor.execute(
-            """
-            CREATE TABLE IF NOT EXISTS tasks (
-                task_id TEXT PRIMARY KEY,
-                priority INTEGER,
-                status TEXT,
-                result TEXT,
-                error TEXT,
-                retry_count INTEGER,
-                access_token TEXT,
-                remote_url TEXT,
-                remote_branch TEXT
-            )
         """
+        Executes the task
+        """
+        from automata.tools.python_tools.python_indexer import PythonIndexer
+        from automata.tools.python_tools.python_writer import PythonWriter
+
+        indexer = PythonIndexer(os.path.join(self.task_dir, self.rel_py_path))
+        writer = PythonWriter(indexer)
+
+        writer.update_module(
+            module_path="core.agent.automata_agent",
+            source_code="def test123(x): return True",
+            write_to_disk=True,
         )
-        self.conn.commit()
+        return None
 
-    def insert_task(self, task: AutomataTask) -> None:
-        cursor = self.conn.cursor()
-        cursor.execute(
-            """
-            INSERT INTO tasks (task_id, priority, status, result, error, retry_count, access_token, remote_url, remote_branch)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """,
-            (
-                task.task_id,
-                task.priority,
-                task.status.value,
-                task.result,
-                task.error,
-                task.retry_count,
-                task.github_manager.remote_url,
-                task.github_manager.remote_branch,
-            ),
+        # overview = indexer.build_overview()
+        # print("Overview = ", overview)
+        # self.status = TaskStatus.RUNNING
+        # try:
+        #     # self.result = self.agent.run()
+        #     self.status = TaskStatus.SUCCESS
+        # except Exception as e:
+        #     logging.exception(f"AutomataTask failed: {e}")
+        #     self.error = str(e)
+        #     self.status = TaskStatus.FAILED
+        #     self.retry_count += 1
+        #     raise
+
+        # return self.result
+
+    def commit_task(
+        self,
+        pull_branch_name: str = "feature/test",
+        commit_message: str = "This is a commit message",
+        pull_title="This is a test",
+        pull_body="I am testing this...",
+    ):
+        """
+        Commits the task to the repository
+        """
+        # Check if the branch already exists, if not create it
+        if not self.github_manager.branch_exists(pull_branch_name):
+            self.github_manager.create_branch(pull_branch_name)
+
+        # Checkout the new branch
+        repo_local_path = self.task_dir
+        self.github_manager.checkout_branch(repo_local_path, pull_branch_name)
+
+        # Stage all changes
+        self.github_manager.stage_all_changes(repo_local_path)
+
+        # Commit and push changes
+        self.github_manager.commit_and_push_changes(
+            repo_local_path, pull_branch_name, commit_message
         )
-        self.conn.commit()
 
-    def update_task(self, task: AutomataTask) -> None:
-        cursor = self.conn.cursor()
-        cursor.execute(
-            """
-            UPDATE tasks SET
-                priority = ?,
-                status = ?,
-                result = ?,
-                error = ?,
-                retry_count = ?,
-                access_token = ?,
-                remote_url = ?,
-                remote_branch = ?
-            WHERE task_id = ?
-        """,
-            (
-                task.priority,
-                task.status.value,
-                task.result,
-                task.error,
-                task.retry_count,
-                task.github_manager.remote_url,
-                task.github_manager.remote_branch,
-                task.task_id,
-            ),
-        )
-        self.conn.commit()
-
-    def get_task(self, task_id: str) -> Optional[AutomataTask]:
-        cursor = self.conn.cursor()
-        cursor.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id,))
-        row = cursor.fetchone()
-
-        if row:
-            github_manager = GitHubManager(
-                access_token=GITHUB_API_KEY, remote_url=row[7], remote_branch=row[8]
-            )
-            task = AutomataTask(github_manager=github_manager)
-            task.task_id = row[0]
-            task.priority = row[1]
-            task.status = TaskStatus(row[2])
-            task.result = row[3]
-            task.error = row[4]
-            task.retry_count = row[5]
-            return task
-        else:
-            return None
-
-
-class TaskRegistry:
-    def __init__(self, db: TaskDatabase):
-        self.db = db
-
-    def add_task(self, task: AutomataTask) -> None:
-        self.db.insert_task(task)
-
-    def get_task(self, task_id) -> Optional[AutomataTask]:
-        return self.db.get_task(task_id)
+        # Create a pull request
+        self.github_manager.create_pull_request(pull_branch_name, pull_title, pull_body)
 
 
 if __name__ == "__main__":
-    print("Running a task...")
     from automata.configs.automata_agent_configs import AutomataAgentConfig
     from automata.configs.config_enums import AgentConfigVersion
 
@@ -231,3 +148,5 @@ if __name__ == "__main__":
         github_manager=github_manager,
     )
     task.create_task_env()
+    task.execute()
+    task.commit_task("test_branch_9")
---
+++ automata/core/task/task_registry.py
@@ -0,0 +1,108 @@
+import sqlite3
+from typing import Optional
+
+from automata.config import GITHUB_API_KEY
+from automata.core.base.github_manager import GitHubManager
+from automata.core.task.task import AutomataTask, TaskStatus
+
+
+class TaskDatabase:
+    def __init__(self, db_name: str):
+        self.conn = sqlite3.connect(db_name)
+        self.create_table()
+
+    def create_table(self) -> None:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS tasks (
+                task_id TEXT PRIMARY KEY,
+                priority INTEGER,
+                status TEXT,
+                result TEXT,
+                error TEXT,
+                retry_count INTEGER,
+                remote_url TEXT,
+                primary_branch TEXT
+            )
+        """
+        )
+        self.conn.commit()
+
+    def insert_task(self, task: AutomataTask) -> None:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            INSERT INTO tasks (task_id, priority, status, result, error, retry_count, remote_url, primary_branch)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+            (
+                task.task_id,
+                task.priority,
+                task.status.value,
+                task.result,
+                task.error,
+                task.retry_count,
+                task.github_manager.remote_name,
+                task.github_manager.primary_branch,
+            ),
+        )
+        self.conn.commit()
+
+    def update_task(self, task: AutomataTask) -> None:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            """
+            UPDATE tasks SET
+                priority = ?,
+                status = ?,
+                result = ?,
+                error = ?,
+                retry_count = ?,
+                remote_name = ?,
+                primary_branch = ?
+            WHERE task_id = ?
+        """,
+            (
+                task.priority,
+                task.status.value,
+                task.result,
+                task.error,
+                task.retry_count,
+                task.github_manager.remote_name,
+                task.github_manager.primary_branch,
+                task.task_id,
+            ),
+        )
+        self.conn.commit()
+
+    def get_task(self, task_id: str) -> Optional[AutomataTask]:
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT * FROM tasks WHERE task_id = ?", (task_id,))
+        row = cursor.fetchone()
+
+        if row:
+            github_manager = GitHubManager(
+                access_token=GITHUB_API_KEY, remote_url=row[7], primary_branch=row[8]
+            )
+            task = AutomataTask(github_manager=github_manager)
+            task.task_id = row[0]
+            task.priority = row[1]
+            task.status = TaskStatus(row[2])
+            task.result = row[3]
+            task.error = row[4]
+            task.retry_count = row[5]
+            return task
+        else:
+            return None
+
+
+class TaskRegistry:
+    def __init__(self, db: TaskDatabase):
+        self.db = db
+
+    def add_task(self, task: AutomataTask) -> None:
+        self.db.insert_task(task)
+
+    def get_task(self, task_id) -> Optional[AutomataTask]:
+        return self.db.get_task(task_id)
