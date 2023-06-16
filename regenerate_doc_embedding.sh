7b3fb4697f25d202cef0b69e755780effac81d2a
Owen <ocolegro@gmail.com>
2023-06-16 10:51:25 -0400

    cleanup embedding infra

--- regenerate_doc_embedding.sh
+++ regenerate_doc_embedding.sh
@@ -1,23 +1,9 @@
-# Build low level docs
-
-## Run this command if we want to update per-method and per-class docstrings
-# python -m automata.cli.scripts.run_docs_l1 --top_n_symbols=100 --update_docs 
-
-## Run this command to constract a map of class symbols to documentation
-python -m automata.cli.scripts.run_docs_l2 --top_n_symbols=2 --update_docs 
-
-## Run this command to aggregate documentation into directory structure
-# doc_gen = PythonDocumentWriter(relative_dir)
-# doc_gen.write_documentation(docs, keys, relative_dir)
-
-# Build aggregate docs
-# ...
-
-# Remove local doc .rst files and re-create
-# ...
-
-# # remove existing docs in the doc repo
-rm -rf ../automata-docs-repo/docs/*
-
-# # copy latest docs over
-cp -rf local_env/docs/* ../automata-docs-repo/docs
+# TODO - Include information on how to configure pyright-scip repository
+source local_env/bin/activate
+node /Users/ocolegrove/scip-python/packages/pyright-scip/index index  --project-name automata --output index_from_fork.scip
+mv index_from_fork.scip automata/config/symbol/index.scip
+# Build and embed the L2 docs
+automata run-doc-embedding-l2
+# Build and embed the L3 docs
+automata run-doc-embedding-l3
+deactivate
--- regenerate_index_and_embeddings.sh
+++ regenerate_index_and_embeddings.sh
@@ -2,5 +2,6 @@
 source local_env/bin/activate
 node /Users/ocolegrove/scip-python/packages/pyright-scip/index index  --project-name automata --output index_from_fork.scip
 mv index_from_fork.scip automata/config/symbol/index.scip
-automata-docs run-code-embedding
+# Update the code embeddings
+automata run-code-embedding
 deactivate
