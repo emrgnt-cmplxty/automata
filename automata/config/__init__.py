3a54c5cf36f788cb98018e2f16160787470bebec
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2023-06-22 16:03:13 -0400

    Add github manager (#50)

--- automata/config/__init__.py
+++ automata/config/__init__.py
@@ -3,7 +3,10 @@
 This file defines environment variables that are used by the program to interact with various APIs, as well as constants that are used throughout the program.
 
 Environment variables:
+- GITHUB_API_KEY: The API key for the GitHub API.
 - OPENAI_API_KEY: The API key for the OpenAI API.
+- CONVERSATION_DB_PATH: The abs path to use for storing conversation data.
+- MAX_WORKERS: The maximum number of workers to run concurrently.
 
 Note that the environment variables are loaded from a .env file using the `load_dotenv()` function from the `dotenv` library.
 """
@@ -15,6 +18,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Define environment variables
+GITHUB_API_KEY = os.getenv("GITHUB_API_KEY", "")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "")
 CONVERSATION_DB_PATH = os.getenv("CONVERSATION_DB_PATH", "interactions.sqlite3")
 MAX_WORKERS = int(os.getenv("MAX_WORKERS", 8))
