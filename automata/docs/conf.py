536e5d9f0833a66b5cf0c9c63eb6bc1da7be112b
Owen <ocolegro@gmail.com>
2023-06-22 09:53:56 -0400

    attempt to modify theme

--- automata/docs/conf.py
+++ automata/docs/conf.py
@@ -24,5 +24,5 @@ root_doc = "index"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "classic"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
