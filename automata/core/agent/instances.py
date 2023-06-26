95ded8b38d35bd945db2ae2c0cc4e2ee711ad29d
Owen <ocolegro@gmail.com>
2023-06-26 16:02:15 -0400

    update import

---
+++ automata/core/agent/instances.py
@@ -0,0 +1,44 @@
+from typing import TYPE_CHECKING, Any, Dict
+
+from pydantic import BaseModel
+
+from automata.config.agent_config_builder import AutomataAgentConfigFactory
+from automata.config.config_types import AgentConfigName
+from automata.core.base.agent import AgentInstance
+
+if TYPE_CHECKING:
+    from automata.core.agent.agents import AutomataOpenAIAgent
+
+
+class AutomataOpenAIAgentInstance(AgentInstance, BaseModel):
+    """An instance of an Automata OpenAI agent."""
+
+    config_name: AgentConfigName = AgentConfigName.DEFAULT
+    description: str = ""
+    kwargs: Dict[str, Any] = {}
+
+    def run(self, instructions: str) -> str:
+        """
+        Executes the specified instructions on an agent built from this instance's configuration
+        and returns the result.
+
+        Args:
+            instructions (str): The instructions to be executed by the agent.
+
+        Returns:
+            str: The output produced by the agent.
+
+        Raises:
+            Exception: If any error occurs during agent execution.
+        """
+        main_config = AutomataAgentConfigFactory.create_config(
+            main_config_name=self.config_name, **self.kwargs
+        )
+
+        agent = AutomataOpenAIAgent(instructions, config=main_config)
+        result = agent.run()
+        del agent
+        return result
+
+    class Config:
+        arbitrary_types_allowed = True
