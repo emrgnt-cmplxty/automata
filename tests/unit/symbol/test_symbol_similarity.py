a3ab080a6733e30535828ea5bbb3d096e07d03d3
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-13 12:29:01 -0500

    Revert test_symbol_similarity

--- tests/unit/symbol/test_symbol_similarity.py
+++ tests/unit/symbol/test_symbol_similarity.py
@@ -15,8 +15,9 @@ from automata.symbol_embedding import (
 )
 
 
-@pytest.fixture
-def embeddings(mock_simple_method_symbols):
+def test_get_nearest_symbols_for_query(
+    monkeypatch, mock_embedding, mock_simple_method_symbols, temp_output_filename
+):
     # Mocking symbols and their embeddings
     symbol1 = mock_simple_method_symbols[0]
     symbol2 = mock_simple_method_symbols[1]
@@ -32,62 +33,38 @@ def embeddings(mock_simple_method_symbols):
         key=symbol3, vector=np.array([0, 0, 1, 0]), document="symbol3"
     )
 
-    return {
-        symbol1: embedding1,
-        symbol2: embedding2,
-        symbol3: embedding3,
-    }
-
-
-@pytest.fixture
-def embedding_db(temp_output_filename, embeddings):
     # Mock JSONSymbolEmbeddingVectorDatabase methods
     embedding_db = JSONSymbolEmbeddingVectorDatabase(temp_output_filename)
-    for embedding in embeddings.values():
-        embedding_db.add(embedding)
+    embedding_db.add(embedding1)
+    embedding_db.add(embedding2)
+    embedding_db.add(embedding3)
 
-    return embedding_db
+    # Create an instance of the class
+    mock_builder = MagicMock(EmbeddingBuilder)
+    cem = SymbolCodeEmbeddingHandler(embedding_db=embedding_db, embedding_builder=mock_builder)
+    mock_provider = MagicMock(EmbeddingVectorProvider)
+    embeddings = {symbol1: embedding1, symbol2: embedding2, symbol3: embedding3}
+    cem.get_embeddings = lambda x: embeddings[x]  # MagicMock(return_value=embeddings[x])
+    symbol_similarity = EmbeddingSimilarityCalculator(mock_provider)
 
-
-@pytest.fixture
-def mock_embedding_builder():
-    return MagicMock(EmbeddingBuilder)
-
-
-@pytest.fixture
-def mock_embedding_vector_provider():
-    return MagicMock(EmbeddingVectorProvider)
-
-
-@pytest.fixture
-def embedding_handler(embedding_db, mock_embedding_builder):
-    cem = SymbolCodeEmbeddingHandler(
-        embedding_db=embedding_db, embedding_builder=mock_embedding_builder
+    # Test with query_text that is most similar to symbol1
+    symbol_similarity.embedding_provider.build_embedding_vector.return_value = np.array(
+        [1, 0, 0, 0]
     )
-    return cem
+    ordered_embeddings = embedding_db.get_ordered_embeddings()
+    result = symbol_similarity.calculate_query_similarity_dict(ordered_embeddings, "symbol1")
+    assert list(result.keys())[np.argmax(list(result.values()))] == symbol1
 
+    # # Test with query_text that is most similar to symbol2
+    symbol_similarity.embedding_provider.build_embedding_vector.return_value = np.array(
+        [0, 1, 0, 0]
+    )
+    result = symbol_similarity.calculate_query_similarity_dict(ordered_embeddings, "symbol2")
+    assert list(result.keys())[np.argmax(list(result.values()))] == symbol2
 
-@pytest.fixture
-def symbol_similarity(mock_embedding_vector_provider):
-    symbol_similarity = EmbeddingSimilarityCalculator(mock_embedding_vector_provider)
-    return symbol_similarity
-
-
-@pytest.mark.parametrize(
-    "symbol_key, query_text",
-    [
-        ("symbol1", np.array([1, 0, 0, 0])),
-        ("symbol2", np.array([0, 1, 0, 0])),
-        ("symbol3", np.array([0, 0, 1, 0])),
-    ],
-)
-def test_get_nearest_symbols_for_query(
-    embedding_handler, symbol_similarity, symbol_key, query_text, embeddings
-):
-    embedding_handler.get_embeddings = lambda x: embedding_handler.embedding_db[x]
-    symbol_similarity.embedding_provider.build_embedding_vector.return_value = query_text
-    ordered_embeddings = embedding_handler.embedding_db.get_ordered_embeddings()
-
-    result = symbol_similarity.calculate_query_similarity_dict(ordered_embeddings, symbol_key)
-
-    assert list(result.keys())[np.argmax(list(result.values()))] == embeddings[symbol_key]
+    # # Test with query_text that is most similar to symbol3
+    symbol_similarity.embedding_provider.build_embedding_vector.return_value = np.array(
+        [0, 0, 1, 0]
+    )
+    result = symbol_similarity.calculate_query_similarity_dict(ordered_embeddings, "symbol3")
+    assert list(result.keys())[np.argmax(list(result.values()))] == symbol3
