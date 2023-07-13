c40d78571ba6d69a4cf6bb6ac2fe19aa4c92119d
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-13 09:39:33 -0500

    Update test_directory_manager.py

--- tests/unit/directory_manager/test_directory_manager.py
+++ tests/unit/directory_manager/test_directory_manager.py
@@ -1,7 +1,8 @@
+import pytest
 from automata.code_parsers import DirectoryManager
 
-
-def create_test_dir_structure(tmp_path):
+@pytest.fixture
+def dir_manager(tmp_path):
     """
     Creates a test directory structure under the given path.
     """
@@ -10,36 +11,28 @@ def create_test_dir_structure(tmp_path):
     (tmp_path / "dir1" / "file1").write_text("content")
     (tmp_path / "dir1" / "file2").write_text("content")
     (tmp_path / "dir2" / "file1").write_text("content")
-    return tmp_path
+    return DirectoryManager(str(tmp_path))
 
 
-def test_load_directory_structure(tmp_path):
-    test_dir = create_test_dir_structure(tmp_path)
-    dir_manager = DirectoryManager(str(test_dir))
-    assert dir_manager.root.name == str(test_dir)
+def test_load_directory_structure(dir_manager):
+    assert dir_manager.root.name == str(dir_manager.path)
     assert len(dir_manager.root.children) == 2  # 2 directories: dir1, dir2
 
 
-def test_get_node_for_path(tmp_path):
-    test_dir = create_test_dir_structure(tmp_path)
-    dir_manager = DirectoryManager(str(test_dir))
+def test_get_node_for_path(dir_manager):
     dir1_node = dir_manager._get_node_for_path(dir_manager.root, "dir1")
     assert dir1_node is not None
     assert dir1_node.name == "dir1"
     assert len(dir1_node.children) == 2  # 2 files in dir1: file1, file2
 
 
-def test_get_files_in_dir(tmp_path):
-    test_dir = create_test_dir_structure(tmp_path)
-    dir_manager = DirectoryManager(str(test_dir))
+def test_get_files_in_dir(dir_manager):
     files_in_dir1 = dir_manager.get_files_in_dir("dir1")
     assert len(files_in_dir1) == 2  # 2 files in dir1: file1, file2
     assert set(files_in_dir1) == {"file1", "file2"}
 
 
-def test_get_subdirectories(tmp_path):
-    test_dir = create_test_dir_structure(tmp_path)
-    dir_manager = DirectoryManager(str(test_dir))
+def test_get_subdirectories(dir_manager):
     subdirectories = dir_manager.get_subdirectories(".")
     assert len(subdirectories) == 2  # 2 subdirectories in root: dir1, dir2
     assert set(subdirectories) == {"dir1", "dir2"}
