439136b24f74ee378c548ee91b6969f5ac8224a5
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-13 11:11:41 -0500

    Update test_symbol_search.py

--- tests/unit/symbol/test_symbol_search.py
+++ tests/unit/symbol/test_symbol_search.py
@@ -34,35 +34,22 @@ def test_exact_search(symbol_search):
         assert result == ["file1", "file2"]
 
 
-def test_process_queries(symbols, symbol_search, symbol_graph_mock):
-    with patch.object(
-        symbol_search, "symbol_references", return_value=["ref1", "ref2"]
-    ) as mock_method_0:
-        result = symbol_search.process_query("type:symbol_references %s" % symbols[0].uri)
-        assert result == ["ref1", "ref2"]
-    mock_method_0.assert_called_once_with(symbols[0].uri)
+@pytest.mark.parametrize(
+    "query_type,method_name,method_return,expected",
+    [
+        ("symbol_references", "symbol_references", ["ref1", "ref2"], ["ref1", "ref2"]),
+        ("exact", "exact_search", {"test": 0}, {"test": 0}),
+        ("source", "retrieve_source_code_by_symbol", "test", "test"),
+        ("symbol_rank", "symbol_rank_search", [("ref1", 0.5), ("ref2", 0.4)], [("ref1", 0.5), ("ref2", 0.4)]),
+    ],
+)
+def test_process_queries(symbols, symbol_search, symbol_graph_mock, query_type, method_name, method_return, expected):
+    with patch.object(symbol_search, method_name, return_value=method_return) as mock_method:
+        result = symbol_search.process_query(f"type:{query_type} {symbols[0].uri}")
+        assert result == expected
+    mock_method.assert_called_once_with(symbols[0].uri)
 
-    with patch.object(symbol_search, "exact_search", return_value={"test": 0}) as mock_method_1:
-        result = symbol_search.process_query("type:exact %s" % symbols[0].uri)
-        assert result == {"test": 0}
-    mock_method_1.assert_called_once_with(symbols[0].uri)
-
-    with patch.object(
-        symbol_search, "retrieve_source_code_by_symbol", return_value="test"
-    ) as mock_method_2:
-        result = symbol_search.process_query("type:source %s" % symbols[0].uri)
-        assert result == "test"
-    mock_method_2.assert_called_once_with(symbols[0].uri)
-
-    with patch.object(
-        symbol_search, "symbol_rank_search", return_value=[("ref1", 0.5), ("ref2", 0.4)]
-    ) as mock_method_4:
-        result = symbol_search.process_query("type:symbol_rank %s" % symbols[0].uri)
-        assert result == [("ref1", 0.5), ("ref2", 0.4)]
-    mock_method_4.assert_called_once_with(symbols[0].uri)
-
+@pytest.mark.parametrize("invalid_query", ["invalid_query", "type:unknown query"])
+def test_process_queries_errors(symbol_search, invalid_query):
     with pytest.raises(ValueError):
-        symbol_search.process_query("invalid_query")
-
-    with pytest.raises(ValueError):
-        symbol_search.process_query("type:unknown query")
+        symbol_search.process_query(invalid_query)
