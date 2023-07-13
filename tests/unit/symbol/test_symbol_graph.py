d73e85704fc09e377727f2cef779faa75da98318
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-13 10:59:18 -0500

    Update test_symbol_graph.py, test_symbol_rank.py, test_symbol_search_tool.py

--- tests/unit/symbol/test_symbol_graph.py
+++ tests/unit/symbol/test_symbol_graph.py
@@ -1,26 +1,27 @@
+import pytest
+
 from automata.context_providers.symbol_synchronization import (
     SymbolProviderSynchronizationContext,
 )
 from automata.singletons.py_module_loader import py_module_loader
 from automata.symbol import Symbol, SymbolGraph
-from tests.utils.factories import symbol_graph_static_test  # noqa: F401
 
 
-def test_get_all_symbols(symbol_graph_static_test):  # noqa: F811
+@pytest.fixture
+def sync_context(symbol_graph_static_test):
     with SymbolProviderSynchronizationContext() as synchronization_context:
         synchronization_context.register_provider(symbol_graph_static_test)
         synchronization_context.synchronize()
+    return synchronization_context
 
+
+def test_get_all_symbols(symbol_graph_static_test, sync_context):
     graph_symbols = symbol_graph_static_test.get_sorted_supported_symbols()
     assert isinstance(graph_symbols, list)
     assert all(isinstance(s, Symbol) for s in graph_symbols)
 
 
-def test_build_real_graph(symbol_graph_static_test):  # noqa: F811
-    with SymbolProviderSynchronizationContext() as synchronization_context:
-        synchronization_context.register_provider(symbol_graph_static_test)
-        synchronization_context.synchronize()
-
+def test_build_real_graph(symbol_graph_static_test, sync_context):
     all_symbols = sorted(
         symbol_graph_static_test.get_sorted_supported_symbols(),
         key=lambda x: x.full_dotpath,
@@ -30,10 +31,7 @@ def test_build_real_graph(symbol_graph_static_test):  # noqa: F811
     assert len(all_symbols) == 1_874
 
 
-def test_build_real_graph_and_subgraph(symbol_graph_static_test):  # noqa: F811
-    with SymbolProviderSynchronizationContext() as synchronization_context:
-        synchronization_context.register_provider(symbol_graph_static_test)
-        synchronization_context.synchronize()
+def test_build_real_graph_and_subgraph(symbol_graph_static_test, sync_context):
     py_module_loader.reset()
     py_module_loader.initialize()
 
