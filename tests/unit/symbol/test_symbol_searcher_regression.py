24389d55072749e97d4e40e262309e41e87f6dd3
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-13 11:26:47 -0500

    Update test_symbol_searcher_regression.py

--- tests/unit/symbol/test_symbol_searcher_regression.py
+++ tests/unit/symbol/test_symbol_searcher_regression.py
@@ -4,13 +4,27 @@ from automata.singletons.py_module_loader import py_module_loader
 from tests.utils.factories import symbol_search_live  # noqa
 
 
+@pytest.fixture
+def initialized_module_loader():
+    py_module_loader.initialized = (
+        False  # This is a hacky way to avoid any risk of initialization error
+    )
+    py_module_loader.initialize()
+    yield py_module_loader
+    py_module_loader.initialized = False
+    py_module_loader.rel_py_path = None
+    py_module_loader.root_fpath = None
+
+
 def get_top_n_results_desc_name(result, n=0):
     return [ele[0].descriptors[-1].name for ele in result[:n]]
 
 
 def check_hits(expected_in_top_hits, found_top_hits):
     for expected_hit in expected_in_top_hits:
-        expected_and_found = any(expected_hit in found_hit for found_hit in found_top_hits)
+        expected_and_found = any(
+            expected_hit in found_hit for found_hit in found_top_hits
+        )
         if not expected_and_found:
             assert (
                 False
@@ -22,7 +36,10 @@ def check_hits(expected_in_top_hits, found_top_hits):
     "search, expected_in_top_hits",
     [
         ("SymbolGraph", ["Symbol", "SymbolGraph", "GraphBuilder"]),
-        ("SymbolSearch", ["Symbol", "SymbolSearchToolkitBuilder", "SymbolSearch"]),
+        (
+            "SymbolSearch",
+            ["Symbol", "SymbolSearchToolkitBuilder", "SymbolSearch"],
+        ),
         (
             "Embedding",
             [
@@ -42,12 +59,23 @@ def check_hits(expected_in_top_hits, found_top_hits):
                 "OpenAIChatCompletionProvider",
             ],
         ),
-        ("LLM", ["LLMProvider", "LLMChatMessage", "LLMConversation", "LLMCompletionResult"]),
+        (
+            "LLM",
+            [
+                "LLMProvider",
+                "LLMChatMessage",
+                "LLMConversation",
+                "LLMCompletionResult",
+            ],
+        ),
         ("Symbol", ["Symbol", "SymbolGraph"]),
     ],
 )
 def test_symbol_rank_search_on_symbol(
-    symbol_search_live, search, expected_in_top_hits  # noqa : F811
+    initialized_module_loader,
+    symbol_search_live,
+    search,
+    expected_in_top_hits,  # noqa : F811
 ):
     py_module_loader.initialized = (
         False  # This is a hacky way to avoid any risk of initialization error
@@ -55,7 +83,9 @@ def test_symbol_rank_search_on_symbol(
 
     py_module_loader.initialize()
     results = symbol_search_live.symbol_rank_search(search)
-    filtered_results = [result for result in results if ".tests." not in result[0].full_dotpath]
+    filtered_results = [
+        result for result in results if ".tests." not in result[0].full_dotpath
+    ]
     found_top_hits = get_top_n_results_desc_name(filtered_results, 10)
     check_hits(expected_in_top_hits, found_top_hits)
 
@@ -94,11 +124,9 @@ EXACT_CALLS_TO_HITS = {
         ),
     ],
 )
-def test_exact_search(symbol_search_live, search, expected_in_hits):  # noqa : F811
-    py_module_loader.initialized = (
-        False  # This is a hacky way to avoid any risk of initialization error
-    )
-    py_module_loader.initialize()
+def test_exact_search(
+    initialized_module_loader, symbol_search_live, search, expected_in_hits
+):  # noqa : F811
     expected_in_exact_hits = EXACT_CALLS_TO_HITS[search]
     found_in_exact_hits = list(symbol_search_live.exact_search(search).keys())
     check_hits(expected_in_exact_hits, found_in_exact_hits)
@@ -117,19 +145,20 @@ def test_exact_search(symbol_search_live, search, expected_in_hits):  # noqa : F
         ),
     ],
 )
-def test_source_code_retrieval(symbol_search_live, search, expected_in_source):  # noqa : F811
-    py_module_loader.initialized = (
-        False  # This is a hacky way to avoid any risk of initialization error
-    )
-    py_module_loader.initialize()
+def test_source_code_retrieval(
+    initialized_module_loader, symbol_search_live, search, expected_in_source
+):  # noqa : F811
     symbols = symbol_search_live.symbol_graph.get_sorted_supported_symbols()
 
-    symbol = [symbol for symbol in symbols if search[:-1] == symbol.descriptors[-1].name][0]
-    found_source_code = symbol_search_live.retrieve_source_code_by_symbol(symbol.uri)
+    symbol = [
+        symbol
+        for symbol in symbols
+        if search[:-1] == symbol.descriptors[-1].name
+    ][0]
+    found_source_code = symbol_search_live.retrieve_source_code_by_symbol(
+        symbol.uri
+    )
     for source_hit in expected_in_source:
         assert (
             source_hit in found_source_code
         ), f"Expected to find {source_hit} in source code, but it was not found"
-    py_module_loader.initialized = False
-    py_module_loader.rel_py_path = None
-    py_module_loader.root_fpath = None
