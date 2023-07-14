27be97c667c61632afefbd7128267245d07472a9
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-14 11:16:33 -0500

    Revert run_code_embedding

--- automata/cli/scripts/run_code_embedding.py
+++ automata/cli/scripts/run_code_embedding.py
@@ -19,7 +19,13 @@ from automata.symbol_embedding import (
 logger = logging.getLogger(__name__)
 
 
-def initialize_resources(project_name, **kwargs):
+def main(*args, **kwargs) -> str:
+    """
+    Update the symbol code embedding based on the specified SCIP index file.
+    """
+    project_name = kwargs.get("project_name") or "automata"
+    initialize_modules(**kwargs)
+
     symbol_graph = SymbolGraph(
         os.path.join(
             DependencyFactory.DEFAULT_SCIP_FPATH, f"{project_name}.scip"
@@ -45,23 +51,15 @@ def initialize_resources(project_name, **kwargs):
     symbol_code_embedding_handler: SymbolCodeEmbeddingHandler = (
         dependency_factory.get("symbol_code_embedding_handler")
     )
-
     # Mock synchronization to allow us to build the initial embedding handler
     symbol_graph.is_synchronized = True
     symbol_code_embedding_handler.is_synchronized = True
 
-    return symbol_graph, symbol_code_embedding_handler
-
-
-def collect_symbols(symbol_graph):
     all_defined_symbols = symbol_graph.get_sorted_supported_symbols()
     filtered_symbols = sorted(
         get_rankable_symbols(all_defined_symbols), key=lambda x: x.full_dotpath
     )
-    return filtered_symbols
 
-
-def process_embeddings(symbol_code_embedding_handler, filtered_symbols):
     for symbol in tqdm(filtered_symbols):
         try:
             symbol_code_embedding_handler.process_embedding(symbol)
@@ -71,16 +69,4 @@ def process_embeddings(symbol_code_embedding_handler, filtered_symbols):
             )
 
     symbol_code_embedding_handler.flush()  # Final flush for any remaining symbols that didn't form a complete batch
-
-
-def main(*args, **kwargs):
-    project_name = kwargs.get("project_name") or "automata"
-    initialize_modules(**kwargs)
-
-    symbol_graph, symbol_code_embedding_handler = initialize_resources(
-        project_name, **kwargs
-    )
-    filtered_symbols = collect_symbols(symbol_graph)
-    process_embeddings(symbol_code_embedding_handler, filtered_symbols)
-
     return "Success"
