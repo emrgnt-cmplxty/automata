e4e7ae0de206d9f55a5713ad6e5eb6053199746b
Owen <ocolegro@gmail.com>
2023-07-15 16:30:50 -0400

    first commit

--- automata/singletons/github_client.py
+++ automata/singletons/github_client.py
@@ -2,7 +2,15 @@ from abc import ABC, abstractmethod
 from typing import Any, List, Optional
 
 from git import Git, Repo
-from github import Github, Issue, PullRequest
+from github import (
+    Github,
+    GitRef,
+    Issue,
+    IssueComment,
+    PaginatedList,
+    PullRequest,
+    PullRequestMergeStatus,
+)
 
 from automata.core.base import Singleton
 
@@ -11,25 +19,25 @@ class RepositoryClient(ABC):
     """An abstract class for managing repositories"""
 
     @abstractmethod
-    def clone_repository(self, local_path: str) -> None:
+    def clone_repository(self, local_path: str) -> Any:
         pass
 
     @abstractmethod
-    def create_branch(self, branch_name: str) -> None:
+    def create_branch(self, branch_name: str) -> Any:
         pass
 
     @abstractmethod
-    def checkout_branch(self, repo_local_path: str, branch_name: str) -> None:
+    def checkout_branch(self, repo_local_path: str, branch_name: str) -> Any:
         pass
 
     @abstractmethod
-    def stage_all_changes(self, repo_local_path: str) -> None:
+    def stage_all_changes(self, repo_local_path: str) -> Any:
         pass
 
     @abstractmethod
     def commit_and_push_changes(
         self, repo_local_path: str, branch_name: str, commit_message: str
-    ) -> None:
+    ) -> Any:
         pass
 
     @abstractmethod
@@ -39,16 +47,18 @@ class RepositoryClient(ABC):
         pass
 
     @abstractmethod
-    def branch_exists(self, branch_name: str) -> bool:
+    def merge_pull_request(
+        self, pull_request_number: int, commit_message: str
+    ) -> PullRequestMergeStatus.PullRequestMergeStatus:
         pass
 
     @abstractmethod
-    def fetch_issue(self, issue_number: int) -> Any:
+    def branch_exists(self, branch_name: str) -> bool:
         pass
 
 
 class GitHubClient(RepositoryClient, metaclass=Singleton):
-    """The GitHub manager provides an interface for interacting with GitHub repositories"""
+    """The GitHub manager provides an interface for interacting with GitHub repositories."""
 
     def __init__(
         self, access_token: str, remote_name: str, primary_branch: str = "main"
@@ -60,7 +70,10 @@ class GitHubClient(RepositoryClient, metaclass=Singleton):
 
         self.primary_branch = primary_branch
 
+    # Repository Manager methods
+
     def clone_repository(self, local_path: str) -> None:
+        """Clone the repository to the local path."""
         # Use Git to clone the repository
         clone_url = self.repo.clone_url.replace(
             "https://",
@@ -69,27 +82,33 @@ class GitHubClient(RepositoryClient, metaclass=Singleton):
 
         Git().clone(clone_url, local_path)
 
-    def create_branch(self, branch_name: str) -> None:
+    def create_branch(self, branch_name: str) -> GitRef.GitRef:
+        """Create a new branch in the repository."""
         # Get the reference to the HEAD commit of the primary_branch
         base_sha = self.repo.get_git_ref(
             f"heads/{self.primary_branch}"
         ).object.sha
         # Create a new branch pointing to the HEAD commit of the primary_branch
-        self.repo.create_git_ref(ref=f"refs/heads/{branch_name}", sha=base_sha)
+        return self.repo.create_git_ref(
+            ref=f"refs/heads/{branch_name}", sha=base_sha
+        )
 
     def checkout_branch(
         self, repo_local_path: str, branch_name: str, b=True
     ) -> None:
+        """Checkout a branch in the repository."""
         repo = Repo(repo_local_path)
         repo.git.checkout(branch_name, b=b)
 
     def stage_all_changes(self, repo_local_path: str) -> None:
+        """Stage all changes in the repository."""
         repo = Repo(repo_local_path)
         repo.git.add(A=True)
 
     def commit_and_push_changes(
         self, repo_local_path: str, branch_name: str, commit_message: str
     ) -> None:
+        """Commit and push all changes in the repository."""
         repo = Repo(repo_local_path)
         repo.git.commit(m=commit_message)
         repo.git.push("origin", branch_name)
@@ -97,23 +116,71 @@ class GitHubClient(RepositoryClient, metaclass=Singleton):
     def create_pull_request(
         self, branch_name: str, title: str, body: str
     ) -> PullRequest.PullRequest:
-        # Create a new pull request on GitHub
+        """Create a new pull request on GitHub."""
         repo = self.client.get_repo(self.remote_name)
         return repo.create_pull(
             title=title, body=body, head=branch_name, base=self.primary_branch
         )
 
-    def create_issue(self, title: str, body: str, labels: List[str]) -> None:
-        # Create a new pull request on GitHub
-        repo = self.client.get_repo(self.remote_name)
-        repo.create_issue(title=title, body=body, labels=labels)
+    def merge_pull_request(
+        self, pull_request_number: int, commit_message: str
+    ) -> PullRequestMergeStatus.PullRequestMergeStatus:
+        """Merge a pull request on GitHub."""
+        pull_request = self.repo.get_pull(number=pull_request_number)
+        return pull_request.merge(commit_message=commit_message)
 
     def branch_exists(self, branch_name: str) -> bool:
+        """Check if a branch exists on GitHub."""
         try:
             self.repo.get_git_ref(f"heads/{branch_name}")
             return True
         except Exception:
             return False
+
+    # Github related methods
+
+    def get_open_issues(self) -> PaginatedList.PaginatedList:
+        """Get the open issues for the remote repository."""
+        return self.repo.get_issues(state="open")
+
+    def get_open_pull_requests(
+        self,
+    ) -> PaginatedList.PaginatedList:
+        """Get the open pull requests for the remote repository."""
+        return self.repo.get_pulls(state="open")
+
+    def create_issue(
+        self, title: str, body: str, labels: List[str]
+    ) -> Issue.Issue:
+        """Create a new issue on GitHub"""
+        repo = self.client.get_repo(self.remote_name)
+        return repo.create_issue(title=title, body=body, labels=labels)
+
+    def create_label(self, issue_number: int, label_name: str) -> None:
+        """Remove a label from an issue on the remote repository."""
+        issue = self.repo.get_issue(number=issue_number)
+        issue.remove_from_labels(label_name)
+
+    def add_label(self, issue_number: int, label_name: str) -> None:
+        issue = self.repo.get_issue(number=issue_number)
+        issue.add_to_labels(label_name)
+
+    def remove_label(self, issue_number: int, label_name: str) -> None:
+        """Remove a label from an issue on the remote repository."""
+        issue = self.repo.get_issue(number=issue_number)
+        issue.remove_from_labels(label_name)
+
+    def create_issue_comment(
+        self, issue_number: int, comment_body: str
+    ) -> IssueComment.IssueComment:
+        """Add a comment to an issue on the remote repository."""
+        issue = self.repo.get_issue(number=issue_number)
+        return issue.create_comment(body=comment_body)
+
+    def remove_issue_comment(self, comment_id: int) -> None:
+        """Remove a comment from an issue on the remote repository."""
+        comment = self.repo.get_comment(comment_id)
+        comment.delete()
 
     def fetch_issue(self, issue_number: int) -> Optional[Issue.Issue]:
         try:
---
+++ tests/unit/github/test_github_client.py
@@ -0,0 +1,285 @@
+import pytest
+import responses
+
+from automata.singletons.github_client import GitHubClient
+
+GITHUB_REQUEST_BASE = "https://api.github.com:443/repos/user/repo"
+GITHUB_URL_BASE = "https://api.github.com/repos/user/repo"
+GITHUB_REQUEST_JSON = {
+    "name": "repo",
+    "owner": {"login": "user"},
+    "url": GITHUB_URL_BASE,
+}
+
+
+@responses.activate
+def test_create_issue():
+    responses.add(
+        responses.GET,
+        GITHUB_REQUEST_BASE,
+        json=GITHUB_REQUEST_JSON,
+        status=200,
+    )
+
+    responses.add(
+        responses.POST,
+        f"{GITHUB_REQUEST_BASE}/issues",
+        json={
+            "number": 1,
+            "title": "Test",
+            "body": "Test issue",
+            "labels": [],
+        },
+        status=201,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    issue = client.create_issue("Test", "Test issue", [])
+
+    assert issue.number == 1
+    assert issue.title == "Test"
+    assert issue.body == "Test issue"
+    assert issue.labels == []
+
+
+@responses.activate
+def test_create_pull_request():
+    responses.add(
+        responses.GET,
+        GITHUB_REQUEST_BASE,
+        json={
+            "name": "repo",
+            "owner": {"login": "user"},
+            "url": GITHUB_URL_BASE,
+        },
+        status=200,
+    )
+
+    responses.add(
+        responses.POST,
+        f"{GITHUB_REQUEST_BASE}/pulls",
+        json={
+            "number": 1,
+            "title": "Test",
+            "body": "Test PR",
+        },
+        status=201,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    pull_request = client.create_pull_request("test-branch", "Test", "Test PR")
+
+    assert pull_request.number == 1
+    assert pull_request.title == "Test"
+    assert pull_request.body == "Test PR"
+
+
+@responses.activate
+def test_merge_pull_request():
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/pulls/1",
+        json={
+            "number": 1,
+            "title": "Test",
+            "body": "Test PR",
+            "url": GITHUB_URL_BASE,
+        },
+        status=200,
+    )
+
+    responses.add(
+        responses.PUT,
+        f"{GITHUB_REQUEST_BASE}/merge",
+        json={
+            "sha": "6dcb09b5b57875f334f61aebed695e2e4193db5e",
+            "merged": True,
+            "message": "Pull Request successfully merged",
+        },
+        status=200,
+    )
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    merge_status = client.merge_pull_request(1, "Merging PR")
+
+    assert merge_status.merged
+    assert merge_status.message == "Pull Request successfully merged"
+
+
+@responses.activate
+def test_get_open_issues():
+    responses.add(
+        responses.GET,
+        GITHUB_REQUEST_BASE,
+        json={
+            "url": f"{GITHUB_URL_BASE}",
+        },
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/issues?state=open",
+        json=[
+            {
+                "number": 1,
+                "title": "Test issue",
+                "body": "This is a test issue",
+                "state": "open",
+            },
+            {
+                "number": 2,
+                "title": "Another test issue",
+                "body": "This is another test issue",
+                "state": "open",
+            },
+        ],
+        status=200,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    issues = client.get_open_issues()
+
+    assert len(list(issues)) == 2
+    assert issues[0].number == 1
+    assert issues[0].title == "Test issue"
+    assert issues[0].body == "This is a test issue"
+    assert issues[0].state == "open"
+
+
+@responses.activate
+def test_branch_exists():
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/git/refs/heads/test-branch",
+        json={
+            "ref": "refs/heads/test-branch",
+            "url": f"{GITHUB_URL_BASE}/git/refs/heads/test-branch",
+        },
+        status=200,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    assert client.branch_exists("test-branch")
+
+
+@responses.activate
+def test_get_open_pull_requests():
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/pulls",
+        json=[
+            {
+                "number": 1,
+                "title": "Test PR",
+                "body": "This is a test PR",
+                "state": "open",
+            },
+            {
+                "number": 2,
+                "title": "Another test PR",
+                "body": "This is another test PR",
+                "state": "open",
+            },
+        ],
+        status=200,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    pull_requests = client.get_open_pull_requests()
+
+    assert len(list(pull_requests)) == 2
+    assert pull_requests[0].number == 1
+    assert pull_requests[0].title == "Test PR"
+    assert pull_requests[0].body == "This is a test PR"
+    assert pull_requests[0].state == "open"
+
+
+@responses.activate
+def test_create_issue_comment():
+    responses.add(
+        responses.GET,
+        GITHUB_REQUEST_BASE,
+        json={
+            "number": 1,
+            "title": "Test issue",
+            "body": "This is a test issue",
+            "state": "open",
+            "url": "https://api.github.com/repos/user/repo",
+        },
+        status=200,
+    )
+
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/issues/1",
+        json={
+            "id": 1,
+            "body": "Test comment",
+            "url": f"{GITHUB_URL_BASE}/issues/1",
+        },
+        status=201,
+    )
+
+    responses.add(
+        responses.POST,
+        f"{GITHUB_REQUEST_BASE}/issues/1/comments",
+        json={
+            "id": 1,
+            "body": "Test comment",
+            "url": f"{GITHUB_URL_BASE}/issues/1/comments",
+        },
+        status=201,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    comment = client.create_issue_comment(1, "Test comment")
+
+    assert comment.id == 1
+    assert comment.body == "Test comment"
+
+
+@pytest.mark.skip(reason="Not implemented")
+@responses.activate
+def test_remove_issue_comment():
+    pass
+
+
+@responses.activate
+def test_fetch_issue():
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/issues/1",
+        json={
+            "number": 1,
+            "title": "Test issue",
+            "body": "This is a test issue",
+            "state": "open",
+        },
+        status=200,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    issue = client.fetch_issue(1)
+
+    assert issue.number == 1
+    assert issue.title == "Test issue"
+    assert issue.body == "This is a test issue"
+    assert issue.state == "open"
+
+
+@responses.activate
+def test_fetch_issue_not_found():
+    responses.add(
+        responses.GET,
+        f"{GITHUB_REQUEST_BASE}/issues/999",
+        json={
+            "message": "Not Found",
+            "documentation_url": "https://docs.github.com/rest/reference/issues#get-an-issue",
+        },
+        status=404,
+    )
+
+    client = GitHubClient("MOCK_TOKEN", "user/repo")
+    issue = client.fetch_issue(999)
+
+    assert issue is None
