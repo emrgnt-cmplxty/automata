acd688c4f76a02da12747f18372c7f1d02df358b
emrgnt-cmplxty <68796651+emrgnt-cmplxty@users.noreply.github.com>
2023-07-20 22:43:06 -0400

    Add test for task w/ db (#270)

--- tests/unit/task/test_task_executor.py
+++ tests/unit/task/test_task_executor.py
@@ -1,13 +1,30 @@
-from unittest.mock import MagicMock
+import os
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from automata.agent import OpenAIAutomataAgent
 from automata.core.utils import get_root_fpath
 from automata.llm import LLMChatMessage
+from automata.memory_store import OpenAIAutomataConversationDatabase
 from automata.singletons.py_module_loader import py_module_loader
 from automata.tasks.base import Task, TaskStatus
-from automata.tasks.executor import AutomataTaskExecutor, ITaskExecution
+from automata.tasks.executor import (
+    AutomataTaskExecutor,
+    IAutomataTaskExecution,
+    ITaskExecution,
+)
+
+
+@pytest.fixture(scope="module", autouse=True)
+def db(tmpdir_factory):
+    db_file = tmpdir_factory.mktemp("data").join("test.db")
+    db = OpenAIAutomataConversationDatabase("session1", str(db_file))
+    yield db
+    db.close()
+    if os.path.exists(str(db_file)):
+        os.remove(str(db_file))
+
 
 mock_message = MagicMock(spec=LLMChatMessage)
 mock_message.role = "assistant"
@@ -71,3 +88,57 @@ def test_execute_automata_task_fail(
 
     assert task.status == TaskStatus.FAILED
     assert task.error == "Execution failed"
+
+
+def mock_openai_response_with_completion_message():
+    return {
+        "choices": [
+            {
+                "message": {
+                    "role": "assistant",
+                    "function_call": {
+                        "name": "call_termination",
+                        "arguments": '{"result": "Success"}',
+                    },
+                    "content": None,
+                }
+            }
+        ]
+    }
+
+
+@pytest.mark.parametrize(
+    "api_response", [mock_openai_response_with_completion_message()]
+)
+@patch("openai.ChatCompletion.create")
+def test_execute_automata_task_with_database_saving(
+    mock_openai_chatcompletion_create,
+    api_response,
+    db,
+    automata_agent,
+    task,
+    environment,
+    registry,
+):
+    # Mock the API response
+    mock_openai_chatcompletion_create.return_value = api_response
+
+    # Create a task and set record_conversation to True
+    registry.register(task)
+    environment.setup(task)
+
+    automata_agent.set_database_provider(db)
+
+    # Execute the task
+    execution = IAutomataTaskExecution()
+    IAutomataTaskExecution._build_agent = MagicMock(
+        return_value=automata_agent
+    )
+    task_executor = AutomataTaskExecutor(execution)
+    task_executor.execute(task)
+
+    assert task.status == TaskStatus.SUCCESS
+    assert task.result == "Execution Result:\n\nSuccess"
+
+    saved_messages = db.get_messages()
+    assert len(saved_messages) == 2
