ff4980faa4d1397037826845c10491ac15fafc51
Nolan Tremelling <34580718+NolanTrem@users.noreply.github.com>
2023-07-24 18:09:45 -0500

    Add project name variable (#293)
    * Add project name
    
    * Allow user to specify vars
    
    * Run black on task_database

--- scripts/regenerate_local_data.sh
+++ scripts/regenerate_local_data.sh
@@ -5,6 +5,29 @@
 # Local environment paths
 : ${EMBEDDING_DATA_PATH:="../automata-embedding-data"}
 
+# Project-specific paths
+# The project name corresponds to the name of the directory in the data_repos directory
+# This should typically correspond to the underlying github repository
+project_name="automata"
+# The directory within the project which contains the python code to be indexed
+# For example, in automata the main python code is located in the automata/ directory
+project_py_dir="automata"
+
+# Set the locale to handle any potential sed illegal byte sequence errors
+# export LC_ALL=C
+
+# Parse command-line arguments
+while getopts ":p:r:" opt; do
+  case ${opt} in
+    p ) project_name=$OPTARG
+      ;;
+    r ) project_py_dir=$OPTARG
+      ;;
+    \? ) echo "Usage: cmd [-p project_name] [-r project_py_dir]"
+      ;;
+  esac
+done
+
 # Remove the old index if it exists
 if [ -f "$EMBEDDING_DATA_PATH/indices/$project_name.scip" ]; then
     rm $EMBEDDING_DATA_PATH/indices/$project_name.scip
