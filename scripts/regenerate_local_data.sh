85358531cb1d06a1b53846f8f365e186e986700b
Owen <ocolegro@gmail.com>
2023-07-26 10:50:40 -0700

    revive broken script

--- scripts/regenerate_local_data.sh
+++ scripts/regenerate_local_data.sh
@@ -1,54 +1,33 @@
 #!/bin/bash
 
-## Default values
-
 # Local environment paths
-: ${EMBEDDING_DATA_PATH:="../automata-embedding-data"}
+: ${PROJECT_NAME:="automata"}
+: ${EMBEDDING_DATA_DIR:="automata-embedding-data"}
+: ${TARGET_DIR:="automata"}
 
-# Project-specific paths
-# The project name corresponds to the name of the directory in the data_repos directory
-# This should typically correspond to the underlying github repository
-project_name="automata"
-# The directory within the project which contains the python code to be indexed
-# For example, in automata the main python code is located in the automata/ directory
-project_py_dir="automata"
-
-# Set the locale to handle any potential sed illegal byte sequence errors
-# export LC_ALL=C
-
-# Parse command-line arguments
-while getopts ":p:r:" opt; do
-  case ${opt} in
-    p ) project_name=$OPTARG
-      ;;
-    r ) project_py_dir=$OPTARG
-      ;;
-    \? ) echo "Usage: cmd [-p project_name] [-r project_py_dir]"
-      ;;
-  esac
-done
 
 # Remove the old index if it exists
-if [ -f "$EMBEDDING_DATA_PATH/indices/$project_name.scip" ]; then
-    rm $EMBEDDING_DATA_PATH/indices/$project_name.scip
+# Note - this assumes that embedding data lives "underneath" scripts dir
+if [ -f "$PWD/../$EMBEDDING_DATA_DIR/indices/$PROJECT_NAME.scip" ]; then
+    rm $PWD/../$EMBEDDING_DATA_DIR/indices/$PROJECT_NAME.scip
 fi
 
-# Change directory to FACTORY_PATH
-mkdir -p $FACTORY_PATH
-cd $FACTORY_PATH
 
-# Copy the project to the factory
-rm -rf $project_name && cp -rf ../$project_name .
-nvm use
-node ../scip-python/packages/pyright-scip/index index --project-name $project_name --output $EMBEDDING_DATA_PATH/indices/$project_py_dir.scip  --target-only $project_py_dir
+# Version Control
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 16.0.0
+nvm use 16.0.0
 
-# Run the project
+# Run the index and embedding generation
 cd ..
-node ./scip-python/packages/pyright-scip/index index --project-name $project_name --target-only $project_name
+echo 'running command', node scip-python/packages/pyright-scip/index index --project-name $PROJECT_NAME --output $EMBEDDING_DATA_PATH/indices/$PROJECT_NAME.scip --target-only $TARGET_DIR
 
-mv index.scip automata-embedding-data/indices/$project_name.scip 
-poetry run $project_name run-code-embedding
-poetry run $project_name run-doc-embedding
+# Note, output path below is relative
+node scip-python/packages/pyright-scip/index index --project-name $PROJECT_NAME --output $EMBEDDING_DATA_DIR/indices/$PROJECT_NAME.scip --target-only $TARGET_DIR
 
-# Return to root
+poetry run automata run-code-embedding
+poetry run automata run-doc-embedding
 cd -
