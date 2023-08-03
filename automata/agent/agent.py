a9d7bc3120113c340b2bce2a023421553f58352c
Owen <ocolegro@gmail.com>
2023-08-03 14:15:24 -0700

    Add python interpreter

--- automata/agent/agent.py
+++ automata/agent/agent.py
@@ -112,6 +112,7 @@ class AgentToolkitNames(Enum):
     PY_WRITER = "py-writer"
     PY_INTERPRETER = "py-interpreter"
     AGENTIFIED_SEARCH = "agent-search"
+    PYTHON_INTERPRETER = "py-interpreter"
 
 
 class AgentToolkitBuilder(ABC):
