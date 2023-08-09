5d31e695ab533a7ff20ef97e8a2a8984ebd36cde
Owen <ocolegro@gmail.com>
2023-08-09 12:51:22 -0700

    Add problem solver

--- research/study_leetcode/leetcode_problems_loader.py
+++ research/study_leetcode/leetcode_problems_loader.py
@@ -35,13 +35,28 @@ class LeetCodeLoader:
         )
         cleaned_snippet = textwrap.dedent(snippet_text)  # type: ignore
 
-        return f"Title:{row['question_title']}\n\nDescription:\n{description_ex_constraints}\n\nNote, your final solution MUST conform to the snippet shown here - ```python\\n{cleaned_snippet}```"
+        return f"Title:{row['question_title']}\n\nDescription:\n{description_ex_constraints}\n\nNote, your final solution MUST BEGIN WITH the snippet shown here - ```python\\n{cleaned_snippet}```"
 
     def get_problem_id_slug(self, idx: int) -> Tuple[int, int, Any]:
         """Retrieve a problem by its index."""
         row = self.data.iloc[idx]
         return (
-            int(row["frontend_question_id"]),
-            int(row["question_id"]),
+            int(row["frontend_question_id"]),  # type: ignore
+            int(row["question_id"]),  # type: ignore
             row["question_slug"],
         )
+
+    def get_problem_slug(self, idx: int) -> Any:
+        """Get the backend problem id for a given problem."""
+        row = self.data.iloc[idx]
+        return str(row["question_slug"])  # type: ignore
+
+    def get_backend_problem_id(self, idx: int) -> int:
+        """Get the backend problem id for a given problem."""
+        row = self.data.iloc[idx]
+        return int(row["question_id"])  # type: ignore
+
+    def get_frontend_problem_id(self, idx: int) -> int:
+        """Get the frontend problem id for a given problem."""
+        row = self.data.iloc[idx]
+        return int(row["frontend_question_id"])  # type: ignore
