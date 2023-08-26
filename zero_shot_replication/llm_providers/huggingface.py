bd47de87b410b0626783eeb315e8fa0c36b333c7
Owen <ocolegro@gmail.com>
2023-08-26 13:39:23 -0700

    tweak hf

--- zero_shot_replication/llm_providers/huggingface.py
+++ zero_shot_replication/llm_providers/huggingface.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 import torch
@@ -5,10 +6,13 @@ from transformers import LlamaForCausalLM, LlamaTokenizer
 
 from zero_shot_replication.llm_providers.base import LLMProvider
 
+logger = logging.getLogger(__name__)
+
 
 class LocalLLamaModel:
     """A class to provide zero-shot completions from a local Llama model."""
 
+    # TODO - Make these upstream configurations
     MAX_OUTPUT_LENGTH = 2048
     TOP_K = 40
 
@@ -20,7 +24,8 @@ class LocalLLamaModel:
         max_output_length=None,
     ) -> None:
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        print(f"Selecting device = {self.device}")
+        logger.info(f"Selecting device = {self.device}")
+
         self.hf_access_token = hf_access_token
         self.max_output_length = (
             max_output_length or LocalLLamaModel.MAX_OUTPUT_LENGTH
@@ -38,12 +43,12 @@ class LocalLLamaModel:
             device_map="auto",
             use_auth_token=self.hf_access_token,
         )
-
         self.temperature = temperature
 
     def get_completion(self, prompt: str, *args, **kwargs) -> str:
         inputs = self.tokenizer(prompt, return_tensors="pt").to("cuda")
 
+        # TODO - Move to HF Configuration approach
         output = self.model.generate(
             inputs["input_ids"],
             temperature=self.temperature,
@@ -51,6 +56,7 @@ class LocalLLamaModel:
             do_sample=True,
             max_new_tokens=self.max_output_length,
         )
+
         output = output[0].to("cpu")
         return self.tokenizer.decode(output)
 
@@ -70,8 +76,11 @@ class HuggingFaceZeroShotProvider(LLMProvider):
 
     def get_completion(self, prompt: str) -> str:
         """Get a completion from the Anthropic API based on the provided prompt."""
-        prompt = f"{prompt}\n### Response:"
+        # TODO - Consider more intelligent place for the '###Response:' string
+        # This was added to match WizardCoder approach exactly.
+        prompt = f"{prompt}\n### Response:\n"
         completion = self.loaded_model.get_completion(prompt)
-        print("completion = ", completion)
-        print("completion, split = ", completion.split(prompt))
+        if prompt in completion:
+            completion = completion.split(prompt)[1]
+        logger.info(f"Found a completion {completion}")
         return completion
